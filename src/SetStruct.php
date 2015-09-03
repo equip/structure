4872a9b1d8ce709d7c2fb94639f024f35dff50e1
Woody Gilk <woody.gilk@gmail.com>
2015-09-02 23:19:29 -0500

    Add set structure

---
+++ src/SetStruct.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Shadowhand\Destrukt;
+
+class SetStruct extends ListStruct
+{
+    public function validate(array $data)
+    {
+        parent::validate($data);
+
+        if (array_unique($data) !== $data) {
+            throw new \InvalidArgumentException(
+                'Set structures must contain only unique values'
+            );
+        }
+    }
+
+    /**
+     * Check if a value exists.
+     *
+     * @param  mixed $value
+     * @return boolean
+     */
+    public function hasValue($value)
+    {
+        return in_array($value, $this->toArray(), true);
+    }
+
+    /**
+     * Get a copy with an new value.
+     *
+     * @throws \UnexpectedValueException if the value already exists
+     * @param  mixed $value
+     * @return self
+     */
+    public function withValue($value)
+    {
+        if ($this->hasValue($value)) {
+            throw new \UnexpectedValueException(
+                'Set already contains the given value'
+            );
+        }
+
+        return parent::withValue($value);
+    }
+}
---
+++ tests/SetTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Shadowhand\Test\Destrukt;
+
+use Shadowhand\Destrukt\SetStruct;
+
+class SetTest extends StructTest
+{
+    public function setUp()
+    {
+        $this->struct = new SetStruct([
+            'red',
+            'green',
+            'blue',
+            'black',
+            'white',
+        ]);
+    }
+
+    public function testExists()
+    {
+        $this->assertTrue($this->struct->hasValue('red'));
+        $this->assertTrue($this->struct->hasValue('white'));
+        $this->assertFalse($this->struct->hasValue('yellow'));
+    }
+
+    public function testReplace()
+    {
+        $set = $this->struct;
+        $copy = $set->withData([
+            'yellow',
+            'orange',
+        ]);
+
+        $this->assertEquals(5, count($set));
+        $this->assertEquals(2, count($copy));
+
+        $this->assertTrue($set->hasValue('white'));
+        $this->assertFalse($copy->hasValue('white'));
+        $this->assertTrue($copy->hasValue('yellow'));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testReplaceFailure()
+    {
+        $this->struct->withData([
+            'black',
+            'blue',
+            'black',
+        ]);
+    }
+
+    public function testAppend()
+    {
+        $set  = $this->struct;
+        $copy = $set->withValue('cyan');
+
+        $this->assertEquals(5, count($set));
+        $this->assertEquals(6, count($copy));
+
+        $this->assertFalse($set->hasValue('cyan'));
+        $this->assertTrue($copy->hasValue('cyan'));
+    }
+
+    /**
+     * @expectedException \UnexpectedValueException
+     */
+    public function testAppendFailure()
+    {
+        $this->struct->withValue('red');
+    }
+
+    public function testUnique()
+    {
+        $set = $this->struct->toArray();
+
+        $this->assertEquals($set, array_unique($set));
+    }
+}
