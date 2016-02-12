e739037dd7c6ae6bbdc3a33360ab0e5ecfbff811
Woody Gilk <woody.gilk@gmail.com>
2016-02-12 14:09:54 -0600

    Make toArray conversion recursive
    Fetching a flat array from a complex structure is often valuable for
    data transmission.

--- src/Traits/CanArray.php
+++ src/Traits/CanArray.php
@@ -2,10 +2,17 @@
 
 namespace Equip\Structure\Traits;
 
+use Equip\Structure\StructureInterface;
+
 trait CanArray /* implements StructureInterface */
 {
     public function toArray()
     {
-        return $this->values;
+        return array_map(static function ($value) {
+            if ($value instanceof StructureInterface) {
+                $value = $value->toArray();
+            }
+            return $value;
+        }, $this->values);
     }
 }
